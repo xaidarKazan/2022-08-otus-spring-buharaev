d64a78903e4166dfbf770dd3226b642dd6acbe23
Aidar <xaidar@list.ru>
2023-01-15 11:51:01 +0300

    ДЗ 12

---
+++ Task_12/src/main/java/ru/otus/homeWork/LibraryApp.java
@@ -0,0 +1,13 @@
+package ru.otus.homeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// http://localhost:8080/
+@SpringBootApplication
+public class LibraryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApp.class, args);
+	}
+}
---
+++ Task_12/src/main/java/ru/otus/homeWork/domain/User.java
@@ -0,0 +1,55 @@
+package ru.otus.homeWork.domain;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "USER")
+@Data
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
